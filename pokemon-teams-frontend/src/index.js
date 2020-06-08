ad2b9fe9245fe0e94fbd1445ce27abed1c9441cd
ewelinaszoda <ewelinaszoda@gmail.com>
2020-06-08 17:57:05 +0200

    first commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,29 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+fetch(TRAINERS_URL)
+.then(resp => resp.json())
+.then(json => console.log(json))
+// .catch(error => console.log(error.message));
+
+
+
+// function parseJSONIntoJSObject(JSONDataAboutTrainers) {
+//     return JSONDataAboutTrainers.json();
+//   }
+
+//  function renderTrainers(trainersData) {
+
+//     showTrainers(trainersData);
+//   }
+//   function handleError(error) {
+//     console.error(error);
+//     alert('YIKES!');
+//   }
+
+//   fetch(TRAINERS_URL)
+//     .then(parseJSONIntoJSObject)
+//     .then(renderTrainers)
+//     .catch(handleError);
